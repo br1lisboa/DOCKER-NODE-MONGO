ca0073870d8ba049d2a7d0e3cd77d8a8990d3baf
br1lisboa <brlisbo@gmail.com>
2024-12-19 12:11:20 -0300

    fix ENV URI

--- src/app.js
+++ src/app.js
@@ -12,13 +12,22 @@ const app = express();
 // es un middleware que se utiliza para parser los json enviados en la request
 app.use(bodyParser.json());
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+console.log("MONGO_DB_NAME:", process.env.MONGO_DB_NAME);
+console.log("MONGO_USER:", process.env.MONGO_USER);
+console.log("MONGO_PASS:", process.env.MONGO_PASS);
+
 // conexión a la BD en mongo
-mongoose.connect(process.env.MONGO_URI, {
-  dbName: process.env.MONGO_DB_NAME,
-  user: process.env.MONGO_USER,
-  pass: process.env.MONGO_PASS,
-});
-const db = mongoose.connection;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    dbName: process.env.MONGO_DB_NAME,
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASS,
+  })
+  .then(() => console.log("Conexión exitosa a la base de datos"))
+  .catch((error) =>
+    console.error("Error al conectar a MongoDB:", error.message)
+  );
 
 // middleware para usar las rutas
 app.use("/techs", techRoutes);
